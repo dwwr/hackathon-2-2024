767f5acfbf7422eadcbb46e37e2d38110040d09d
dwwr <derek.warnerreyes@olioapps.com>
2024-02-18 13:43:28 -0800

    init, spike feature

---
+++ src/content.ts
@@ -0,0 +1,11 @@
+// 'use strict'
+
+// console.log('extension script running')
+
+// let audibleElements = document.getElementsByTagName('audio')
+// let canvasElements = document.getElementsByTagName('canvas')
+// let videoElements = document.getElementsByTagName('video')
+
+// console.log(audibleElements)
+// console.log(canvasElements)
+// console.log(videoElements)
